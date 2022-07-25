e3b9634dd2643495c4736774b7e8997225600f0c
mustafa <mhaidarpoor@gmail.com>
2022-07-25 16:59:44 +1000

    first commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const connnection = require('./db');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth')
+
+// database connection
+connnection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use('/api/users', userRoutes);
+
+
+const port = process.env.Port || 8080;
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
